e2c80d05b146ee40bc9e61714c4d7d48a448ed04
wasu <wasuwa911@gmail.com>
2025-01-23 02:06:20 +0900

    ミドルウェアを設定する

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/wasuwa/night-view-api/api"
 	"github.com/wasuwa/night-view-api/handler"
 	"github.com/wasuwa/night-view-api/infrastructure/datastore"
@@ -19,6 +21,21 @@ func main() {
 	server := api.NewServer(nightViewHandler)
 
 	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(
+		middleware.CORS(),
+		middleware.Logger(),
+		middleware.Recover(),
+		middleware.CSRF(),
+		middleware.RequestID(),
+		middleware.Secure(),
+		middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+			Skipper:      middleware.DefaultSkipper,
+			Timeout:      time.Minute,
+			ErrorMessage: "timeout",
+		}),
+	)
+
 	api.RegisterHandlers(e, server)
 
 	log.Fatal(e.Start(":8080"))
